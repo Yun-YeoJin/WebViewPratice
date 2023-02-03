84f71ffaacb2a593badbc8b3b8e9beb8778e1700
yeojin <duwls0349@naver.com>
2023-02-03 18:21:41 +0900

    [Feat] Safari ViewModel 생성 Input / Output 패턴 구현

---
+++ WebViewPratice/Presentation/SafariScene/ViewModel/SafariViewModel.swift
@@ -0,0 +1,79 @@
+//
+//  SafariViewModel.swift
+//  WebViewPratice
+//
+//  Created by YEOJIN on 2023/02/03.
+//
+
+import Foundation
+
+import RxCocoa
+import RxSwift
+
+class SafariViewModel: ViewModelType {
+    
+    var disposeBag = DisposeBag()
+    
+    var newsList: [NewsItems] = []
+    
+    struct Input {
+        let viewDidLoadEvent: Observable<Void>
+        let querySubject: PublishSubject<String>
+        let sortSubject: PublishSubject<String>
+        let startPageSubject: PublishSubject<Int>
+        let searchButtonTap: ControlEvent<Void>
+    }
+    
+    struct Output {
+        var newsData: PublishSubject<[NewsSectionModel]>
+    }
+    
+    func transform(input: Input) -> Output {
+        
+        let newsData = PublishSubject<[NewsSectionModel]>()
+        
+        input.viewDidLoadEvent
+            .observe(on: MainScheduler.instance)
+            .subscribe { [weak self] _ in
+                newsData.onNext([])
+                self?.newsList = []
+            }.disposed(by: disposeBag)
+        
+        input.searchButtonTap
+            .observe(on: MainScheduler.instance)
+            .subscribe { [weak self] _ in
+                self?.newsList = []
+                input.startPageSubject.onNext(0)
+            }.disposed(by: disposeBag)
+        
+        input.querySubject
+            .observe(on: SerialDispatchQueueScheduler(qos: .background))
+            .flatMap { query -> Observable<[NewsItems]> in
+                let observable = self.requestNewsAPI(query: query, display: 20, start: 1, sort: "sim")
+                return observable
+            }
+            .subscribe { newsDetail in
+                self.newsList.append(contentsOf: newsDetail)
+                newsData.onNext([NewsSectionModel(items: self.newsList)])
+            }.disposed(by: disposeBag)
+        
+        return Output(newsData: newsData)
+    }
+    
+    func requestNewsAPI(query: String, display: Int, start: Int, sort: String) -> Observable<[NewsItems]> {
+        
+        return Observable<[NewsItems]>.create { observer in
+            APIService.shared.requestNewsAPI(type: NaverNewsDTO.self, router: NaverNewsRouter.searchNaverNews(query: query, display: "\(display)", start: "\(start)", sort: sort)) { result in
+                switch result {
+                case .success(let success):
+                    observer.onNext(success.items)
+                    observer.onCompleted()
+                case .failure(let error):
+                    observer.onError(error)
+                }
+            }
+            return Disposables.create()
+        }.observe(on: SerialDispatchQueueScheduler(qos: .background))
+    }
+    
+}
